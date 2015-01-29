a8d92db8dd74545f7b10a4fe16b832db229064b1
Alex Cross <abcross@calpoly.edu>
2015-01-29 15:56:07 -0800

    weather clock changes

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,6 @@
 function getTime(){
 	var date = new Date();
-	var h = date.getHours();
+	var h = date.getHours() % 12;
 	var m = date.getMinutes();
 	var s = date.getSeconds();
 	
@@ -15,7 +15,7 @@ function getTime(){
 		
 	}
 	
-	document.getElementById("clock").innerHTML = time;
+	$("#clock").html(time);
 	setTimeout(getTime, 1000);
 }
 
@@ -25,4 +25,47 @@ function alignTime(t){
 	}
 	
 	return t;
+}
+
+function getTemp(){
+	var imgSrc = "img/";
+	var maxTemp;
+	
+	$.getJSON("https://api.forecast.io/forecast/c173b25a2b8409c9b60653479a83787c/35.300399,-120.662362?callback=?", function(json){
+	
+		$("#forecastLabel").html(json.daily.summary);
+		
+		imgSrc = imgSrc + json.daily.icon + ".svg";
+		$("#icon").attr('src', imgSrc);
+		
+		maxTemp = json.daily.data[0].temperatureMax;
+		setColor(maxTemp);
+	});
+}
+
+function setColor(temp){
+	if(temp < 60){
+		$("body").addClass("cold");
+	}
+	
+	else if(temp < 70){
+		$("body").addClass("chilly");
+	}
+	
+	else if(temp < 80){
+		$("body").addClass("nice");
+	}
+	
+	else if(temp < 90){
+		$("body").addClass("warm");
+	}
+	
+	else{
+		$("body").addClass("hot");
+	}
+}
+
+function load(){
+	getTime();
+	getTemp();
 }
